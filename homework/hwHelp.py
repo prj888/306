e82393c888cd9675bb3018c3169b8b491755662d
prj888 <m213126@usna.edu>
2020-02-18 08:36:10 -0500

    wrt

--- homework/hwHelp.py
+++ homework/hwHelp.py
@@ -3,6 +3,35 @@ cgitb.enable()
 
 input_data = cgi.FieldStorage()
 
-print('Content-Type: text/html') # HTML is following
-print()                         # Leave a blank line
-print('<h1>Addition Results</h1>')
+firstName = form.getvalue("firstname")
+lastName = form.getvalue("lastname")
+
+#generate output as feedback for the user
+print ("Content-Type: text/html")
+print ()
+print ('''\
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset = "utf-8">
+<title>Survey Feedback</title>
+</head>
+<body>
+''')
+
+if firstName == None or lastName == None:
+  print ('<p>Please fill out both food AND color</p>')
+else:
+  # Save result in file.  Use colon as separator
+  outfile = open ("names.txt","a")
+  outfile.write(first + " : " + last + "\n")
+  outfile.close()
+
+  print ('<h1>Thank you for filling out our survey</h1>')
+  print ('<p>Your responses have been recorded as follows:</p>')
+  print ('<ul>')
+  print ('<li>First Name: ' + firstName + '</li>')
+  print ('<li>Last Name: ' + lastName + '</li>')
+  print ('</ul>')
+
+print ('</body></html>')
