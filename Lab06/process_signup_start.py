02fb52541f98963d28023223594d43b8b96291d2
Paul VM <m213126@usna.edu>
2020-02-23 19:40:07 -0500

    d

---
+++ Lab06/process_signup_start.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+import cgi
+import cgitb; cgitb.enable()
+
+form = cgi.FieldStorage()
+
+print ("Content-Type: text/html")
+print ()
+print ('''\
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset = "utf-8">
+<title>Survey Feedback</title>
+</head>
+<body>
+''')
+
+def main():
+	error = "AN ERROR OCCURRED!"
+	try:
+		firstName = str(form.getvalue("firstname"))
+		lastName = str(form.getvalue("lastname"))
+		gender = str(form.getvalue("gender"))
+		email = str(form.getvalue("email"))
+		password = str(form.getvalue("pass"))
+		comments = str(form.getvalue("comments"))
+	except:
+		print(error)
+		return False
+	
+	print("SUCCESS! Thank you for signing up for an outdoor membership!")
+	print("The email you signed up with is ",email)
+	
+main()
+
+print ('''\
+</body>
+</html>
+''')
