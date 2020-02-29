e89a7097c51e446eb5f63a9e8ba731ff0b61da8e
Paul VM <m213126@usna.edu>
2020-02-28 23:23:17 -0500

    fdaf

--- Lab06/process_signup_check.py
+++ Lab06/process_signup_check.py
@@ -39,73 +39,54 @@ def main():
 	# Validate Text Portion
 	test = True
 	append = False
-	print("CHECK POINT 1")
 	if (firstName == "") or firstName == 'None':
 		print("The first name box was unfilled, membership not complete please return to previous page!")
 		test = False
-		print("IF")
 	else:
-		print("ELSE")
 		pass
-	print("CHECK POINT 2")
 	# Validate textarea
 	if (comments == "") or comments == 'None':
 		print("The comments box was unfilled, membership not complete please return to previous page!")
 		test = False
-		print("IF)")
 
 	# Validate radio buttons
 	elif (gender == "") or gender == 'None':
 		print("The gender selection was unfilled, membership not complete please return to previous page!")
 		test = False
-		print("ELIF")
 		# Validate password
 	elif (password == "") or password == 'None':
 		print("The password box was unfilled, membership not complete please return to previous page!")
 		test = False
 	else:
-		print("ELSE")
 		digit = False
 		for i in password:
-			print("for")
 			if i.isdigit():
-				print("if")
 				digit = True
 				break
 			else:
-				print("else")
 				digit = False
-		print("CHECK POINT 3")
 		if digit == False:
 			print("The password entered did not contain a number, membership not complete please return to previous page!")
 			test = False
 		elif digit == True:
-			print("elif")
 			test = True
-			print("TEST: ",test)
-	print("CHECK POINT 4")
 	if test == True:
-		print("IF")
 		first = usersFile.read(1)
 		if not first:
 			append = True
 		else:
 			pass
 		for line in usersFile:
-			print("for")
 			userInfo = line.split()
 			if userInfo[3] == email:
 				print("A registration already exists for the email that was entered, if this is an issue please return to the previous page.")
 				append = False
 			else:
-				print("else")
 				append = True
-				print("APPEND: ",append)
 				
 	elif test == False:
 		append = False
 		return False
-	print("CHECK POINT 5")
 	if append == True:
 		print('<img src="Mountain_Range.jpg" style="width:100%;" alt="Mountain Range">')
 		print("<h1 style=text-align:center;> Paul's Outdoor Recreation</h1>")
@@ -137,11 +118,9 @@ def main():
 		string = firstName+'\t'+lastName+'\t'+gender+'\t'+email+'\t'+password+'\t'+comments+'\n'
 		usersFile.write(string)
 	elif append == False:
-		print("elif)")
 		return False
 
 
-	print("CHECK POINT 5")
 	#ALWAYS EXECUTE
 	usersFile.close()
 
